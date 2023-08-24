5c3c7037f764065e0135bccbda590d8ab5a833d5
Pere Joan Martorell <pj.martorell@gmail.com>
2023-08-25 01:06:45 +0200

    Remove dblTap event from CanvasItem as it is already covered by dblClick event

--- src/InitialOverlayDialog.js
+++ src/InitialOverlayDialog.js
@@ -24,7 +24,7 @@ const InitialOverlayDialog = () => {
             <div className="dialog-content">
                 <h2>Primeros pasos!</h2>
                 <p>Para diseñar una formación para tu bolo, dirígete al menú principal y selecciona la opción "Percusionistas". En la ventana emergente, elige a las personas que deseas añadir o eliminar de tu diseño. Las personas seleccionadas se añadirán al lienzo y podrás moverlas libremente arrastrándolas.</p>
-                <p>Si deseas eliminar a una persona directamente desde el lienzo, realiza un doble clic sobre la persona (en caso de un navegador de escritorio) o un toque (en caso de un navegador móvil). Aparecerá un menú en el que debes seleccionar la opción de "Eliminar".</p>
+                <p>Si deseas eliminar a una persona directamente desde el lienzo, realiza un doble clic sobre la persona. Aparecerá un menú en el que debes seleccionar la opción de "Eliminar".</p>
                 <p>Si deseas cambiar el instrumento de una persona, sigue los mismos pasos anteriores y en el menú emergente selecciona "Cambiar instrumento". Aparecerá una ventana emergente donde podrás elegir el instrumento que deseas asignarle a esa persona.</p>
                 <p>Por último, si deseas exportar el diseño como una imagen, ve al menú principal y selecciona la opción "Exportar imagen".</p>
 
