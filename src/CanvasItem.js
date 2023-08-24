547477dd5c973c5c24133edce41aff0bbd600999
Pere Joan Martorell <pj.martorell@gmail.com>
2023-08-24 13:35:17 +0200

    Add bounding circle and rectangle with gradient color and adapted to the size of the text

--- src/CanvasItem.js
+++ src/CanvasItem.js
@@ -1,4 +1,4 @@
-import {Circle, Group, Image, Text} from 'react-konva';
+import {Circle, Group, Image, Rect, Text} from 'react-konva';
 import useImage from 'use-image';
 import React from "react";
 
@@ -8,6 +8,11 @@ const CanvasItem = ({ x, y, musician_name, musician_instrument, imageUrl, onDrag
     const circleWidth = circleRadius * 2;
     const imageWidth = 80;
     const imageHeight = 80;
+    const fontSize = 12;
+    const padding = 4;
+    // Calculate the width and height needed for the rectangle to bound the text
+    const textWidth = musician_name.length * fontSize * 0.6;
+    const textHeight = fontSize;
 
     return (
         <Group x={x} y={y} draggable onDragMove={onDragMove} onDragStart={onDragStart} onDragEnd={onDragEnd} onContextMenu={onContextMenu}>
@@ -33,8 +38,52 @@ const CanvasItem = ({ x, y, musician_name, musician_instrument, imageUrl, onDrag
                        y={-imageHeight / 2} // Center the image vertically
                 />
             </Group>
-            <Text text={musician_name} x={-circleRadius} y={-circleRadius} align="center" width={circleWidth} />
-            <Text text={musician_instrument} x={-circleRadius} y={circleRadius - 10} align="center" width={circleWidth} />
+            <Group>
+                <Rect
+                    x={ - textWidth / 2 - padding}
+                    y={-circleRadius - 5 - padding}
+                    width={textWidth + padding * 2}
+                    height={textHeight + padding * 2}
+                    cornerRadius={10}
+                    fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+                    fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+                    fillLinearGradientColorStops={[0.3, 'blue', 1, 'aquamarine']}
+                    // fillLinearGradientColorStops={[0.1, 'red', 1, 'yellow']}
+                    // shadowColor="black"
+                    // shadowBlur={10}
+                    // shadowOpacity={0.6}
+                />
+                <Text
+                    text={musician_name}
+                    x={-circleRadius}
+                    y={-circleRadius - 5}
+                    align="center"
+                    width={circleWidth}
+                    fill="white"
+                />
+            </Group>
+            <Group>
+                <Circle
+                    // fill="blue"
+                    x={0}
+                    y={circleRadius}
+                    radius={10}
+                    fillLinearGradientStartPoint={{ x: 0, y: circleRadius }} // Bottom center of the circle
+                    fillLinearGradientEndPoint={{ x: 0, y: -circleRadius }} // Top center of the circle
+                    fillLinearGradientColorStops={[0.3, 'blue', 1, 'aquamarine']}
+                    // shadowColor="black"
+                    // shadowBlur={10}
+                    // shadowOpacity={0.6}
+                />
+                <Text
+                    text={musician_instrument}
+                    x={-circleRadius}
+                    y={circleRadius - 5}
+                    align="center"
+                    width={circleWidth}
+                    fill="white"
+                />
+            </Group>
         </Group>
     );
 };
